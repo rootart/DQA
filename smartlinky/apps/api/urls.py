ce0cca44c61e598f4504d645489f4a88455b585c
virtuallight <mat.jankowski@gmail.com>
2011-07-30 20:19:48 +0200

    adding comments removing demo views

--- smartlinky/apps/api/urls.py
+++ smartlinky/apps/api/urls.py
@@ -4,12 +4,7 @@ from django.conf.urls.defaults import patterns, url
 urlpatterns = patterns('apps.api.views', 
     url(r'^init$', 'init', name='api_init'),
     url(r'^user_links$', 'user_links', name='api_user_links'),
-    url(r'^qa_links$', 'qa_links', name='api_qa_init'),
+    url(r'^qa_links$', 'qa_links', name='qa_links'),
     url(r'^add_link$', 'add_link', name='api_add_link'),
-
-    # TODO: remove when proper urls work
-    url(r'^demo/init$', 'demo_init', name='api_demo_init'),
-    url(r'^demo/user_links$', 'demo_user_links', name='api_demo_user_links'),
-    url(r'^demo/qa_links$', 'demo_qa_links', name='api_demo_qa_init'),
 )
 
