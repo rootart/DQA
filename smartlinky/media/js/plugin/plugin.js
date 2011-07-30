9c5e6100edc95628bbaed836ad49ece9e386632d
Attila Maczak <attila@maczak.hu>
2011-07-30 10:41:01 +0200

    Working Parser

--- smartlinky/media/js/plugin/plugin.js
+++ smartlinky/media/js/plugin/plugin.js
@@ -1,15 +1,16 @@
 
 (function($){$(function(){
     var parser = new Parser();
+    parser.parse();
     //Initialize
     $.ajax({
         url: '%%api-url%%',
         data: {
-            url: window.location.href
+            url: window.location.href,
+            sections: parser.sections
         },
         crossDomain: true,
         success: function(){
-            var             
         } 
     });
 });})($);
