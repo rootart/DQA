d67f2cd00b131f0cdd715c4221493531b43401bd
virtuallight <mat.jankowski@gmail.com>
2011-07-30 19:32:55 +0200

    handling exceptions in api views

--- smartlinky/apps/api/decorators.py
+++ smartlinky/apps/api/decorators.py
@@ -1,17 +1,28 @@
 from functools import wraps
+from pprint import pprint
+import logging
 
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseBadRequest
 from django.utils import simplejson as json
 
+
+logger = logging.getLogger(__name__)
 
 def xss_json_response(f):
     """Dump a function result into a json, wrap with HttpResponse and make xss friendly."""
     
     @wraps(f)
     def wrapper(*args, **kwargs):
-        content = f(*args, **kwargs)
-        response = HttpResponse(json.dumps(content), mimetype='application/json')
-        response['Access-Control-Allow-Origin']  = '*'
-        response['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS, PUT, DELETE' 
-        return response
+        try:
+            content = f(*args, **kwargs)
+        except Exception, e:
+            logger.error('{%s@xss_json_response} %s' % (f.__name__, pprint(e)))
+            content = {'message': pprint(e)}
+            response = HttpResponseBadRequest(json.dumps(content), mimetype='application/json')
+        else:    
+            response = HttpResponse(json.dumps(content), mimetype='application/json')
+        finally:
+            response['Access-Control-Allow-Origin']  = '*'
+            response['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS, PUT, DELETE' 
+            return response
     return wrapper
