6d094a7e3d00724875b9a3d5edda99ed8f5b7249
Attila Maczak <attila@maczak.hu>
2011-07-30 18:58:55 +0200

    Add get_page_title util func; New api call: new_link

--- smartlinky/apps/api/views.py
+++ smartlinky/apps/api/views.py
@@ -1,4 +1,8 @@
+from django.http import HttpResponseBadRequest
 from decorators import xss_json_response
+
+from apps.core.models import Page, Section, Link
+from apps.utils.utils import get_page_title
 
 # TODO: remove when proper api views are working
 from views_demo import demo_init, demo_user_links, demo_qa_links
@@ -75,3 +79,52 @@ def qa_links(request):
     html_id = request.GET['id']
     html_title = request.GET['section_title']
     return content
+
+
+@xss_json_response
+def new_link(request):
+    """Create a new link. If the section and page don't exist, it'll create them.
+
+    :param page_title: Title of documentation's page
+    :type page_title: str
+
+    :param url: URL of documentation's page
+    :type url: str
+
+    :param section: HTML ID of section
+    :type section: str
+
+    :param section_title: Title of section
+    :type section_title: str
+
+    :param link_url: URL of new link
+    :type link_url: str
+    """
+    page_title = request.POST['page_title']
+    url = request.POST['url']
+    section = request.POST['section']
+    section_title = request.POST['section_title']
+    link_url = request.POST['link_url']
+
+    try:
+        # Fetch & parse the linked page
+        link_title = get_page_title(link_url)
+    except:
+        return HttpResponseBadRequest()
+
+    try:
+        section = Section.objects.get(html_id=section, page__url=url)
+    except Section.DoesNotExist:
+        try:
+            page = Page.objects.get(url=url)
+        except Page.DoesNotExist:
+            # Create page
+            page = Page.objects.create(url=url, meta_title=page_title)
+        # Create section     
+        section = Section.objects.create(html_id=section, 
+            html_title=section_title, page=page)
+
+    # Create link
+    Link.objects.create(url=link_url, title=link_title, section=section)
+
+
