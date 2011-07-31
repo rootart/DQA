6244628026404ac3d3940e1aa30e9ed4a89dd58e
Attila Maczak <attila@maczak.hu>
2011-07-31 23:11:14 +0200

    Example page

--- smartlinky/apps/site/urls.py
+++ smartlinky/apps/site/urls.py
@@ -5,6 +5,7 @@ from django.views.generic.simple import direct_to_template
 urlpatterns = patterns('apps.site.views', 
     url(r'^about$', 'about', name='site_about'),
     url(r'^search$', 'search', name='site_search'),
+    url(r'^example$', direct_to_template, {'template': 'site/example.html'}, name='site_example'),
     url(r'^$', 'index', name='site_index'),
 )
 
