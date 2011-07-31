5edd50d6fe4ef1918ad7f5563a5da0a492a4c721
Attila Maczak <attila@maczak.hu>
2011-07-31 13:52:36 +0200

    Nice plugin user widget

--- smartlinky/site_media/media/js/plugin/style.js
+++ smartlinky/site_media/media/js/plugin/style.js
@@ -14,19 +14,25 @@ var cleaner = {
 
 var style = {
     'wrapper': {
+        height: '0px',
+        position: 'relative'
+    },
+    'container': {
 	width: '314px',
 	position: 'absolute',
+        right: '-60px',
 	fontFamily: 'Arial, "Helvetica", sans-serif',
 	fontSize: '11px'
     },
     'button': {
-	'float':'left',
+	'float': 'right',
 	background: 'url("{{media-url}}imgs/icon_widget.png") no-repeat',
 	height: '34px',
 	width: '16px',
 	paddingLeft: '26px',
 	color: '#fff',
-	margin: '10px'
+	margin: '10px',
+        cursor: 'pointer'
     },
     'list': {
 	color: '#404646',
@@ -40,7 +46,7 @@ var style = {
 	textTransform: 'none',
 	marginTop: '0px'
     },
-    'container': {
+    'widget': {
 	background: '#f3f3f3',
 	border: '1px solid #bebebe',
 	'-webkit-box-shadow': '0px 0px 5px 0px #4a4a4a',
@@ -61,18 +67,45 @@ var style = {
 	listStyle: 'none'
     },
     'user_links': {
-	background: '#85d1e9',
-	border: '1px solid #46b0d1'
+        paddingTop: '4px',
+        paddingLeft: '8px'
     },
     'user_links_element': {
 	borderBottom: '1px solid #46b0d1'
     },
-//    'user_links_more':
-//    'title_user':
+    'user_links_more': {
+        background: 'none repeat scroll 0 0 #46B0D1',
+        'border-bottom-left-radius': '5px',
+        'border-bottom-right-radius': '5px',
+        fontWeight: 'bold',
+        marginTop: '-9px',
+        padding: '6px 10px',
+        'float': 'right'
+    },
+    'userlinks_title': {
+        'border-top-left-radius': '5px',
+        'border-top-right-radius': '5px',
+        'font-weight': 'bold',
+        'padding': '6px 10px 11px',
+        'text-transform': 'uppercase',
+        'border-left': '1px solid #46B0D1',
+        'border-right': '1px solid #46B0D1',
+        'border-top': '1px solid #46B0D1',
+        'background': 'none repeat scroll 0 0 #85D1E9',
+        'display': 'inline' 
+    },
+    'userlinks_list': {
+        background: 'none repeat scroll 0 0 #85D1E9',
+        border: '1px solid #46B0D1',
+        paddingTop: '4px',
+        paddingLeft: '1.2em'
+    },
     'add_button': {
 	color: '#fff',
 	background: '#d94545',
-	padding: '2px 5px'   
+	padding: '2px 5px',
+        display: 'inline',
+        'float': 'right'
     },
 //    'star':
     'link': {
@@ -94,6 +127,6 @@ var style = {
 
 for (var key in style) {
     if (style.hasOwnProperty(key)) {
-        style[key] = $.extend(cleaner, style[key]);
+        style[key] = $.extend({}, cleaner, style[key]);
     }
 }
