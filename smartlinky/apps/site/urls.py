91bf0885c8e15c31f0b9164ea14a542d771a5356
virtuallight <mat.jankowski@gmail.com>
2011-07-31 23:09:22 +0200

    changes...

--- smartlinky/apps/site/urls.py
+++ smartlinky/apps/site/urls.py
@@ -6,6 +6,8 @@ urlpatterns = patterns('apps.site.views',
     url(r'^about$', 'about', name='site_about'),
     url(r'^search$', 'search', name='site_search'),
     url(r'^$', 'index', name='site_index'),
+    # TODO: create an example site with docs and Smartlinky functionality embedded
+    url(r'^$', 'index', name='site_example'),
 )
 
 
